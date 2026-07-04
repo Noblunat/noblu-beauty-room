3658fbf208f9e81a169e3609b52bd733b0ba9bb8
Natalia <noblu.beautyroom@gmail.com>
2026-07-04 20:14:41 +0200

    Add Vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import ConversionEvents from "./components/ConversionEvents";
 import CookieBanner from "./components/CookieBanner";
 import GoogleTag from "./components/GoogleTag";
@@ -207,6 +209,8 @@ export default function RootLayout({
         <GoogleTag />
         <ConversionEvents />
         <CookieBanner />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
